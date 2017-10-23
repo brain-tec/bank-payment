ec3d1e72a5a683f089f194535c8c9e0e6e2b2091
Olivier Jossen <olivier.jossen@brain-tec.ch>
2017-10-23 09:06:28 +0200

    removed wrong comments

--- account_banking_pain_base/migrations/9.0.9.0.0/pre-migration.py
+++ account_banking_pain_base/migrations/9.0.9.0.0/pre-migration.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2015 Yannick Vaucher (Camptocamp SA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-"""
-The banks have been created with dumb xml ids.
-Change xml ids to keep track of those bank with
-http://www.six-interbank-clearing.com/ (see README.rst for source)
-"""
-
 
 def migrate(cr, version):
     if not version:
